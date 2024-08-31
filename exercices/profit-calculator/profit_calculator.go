371ed95b8196c58de765fd00e9d1c12ff42e50c9
Phil <alexis.hochart@milixis.fr>
2024-08-31 15:12:38 +0200

    profit calc exercice + repo architecture change

---
+++ exercices/profit-calculator/profit_calculator.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var revenues, expenses, taxRate float64
+
+	fmt.Print("Enter your revenues: ")
+	fmt.Scan(&revenues)
+
+	fmt.Print("Enter your expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	EBT := revenues - expenses
+	fmt.Println("Raw earning: ", EBT)
+
+	profit := EBT - (EBT * taxRate / 100)
+	ratio := EBT / profit
+
+	fmt.Println("Profit: ", profit)
+	fmt.Println("Ratio: ", ratio)
+
+}
