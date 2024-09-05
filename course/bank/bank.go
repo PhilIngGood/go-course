a5615fc4adf3bc3d0050237f9417f7066e1a3e56
phil <alexis.hochart@milixis.fr>
2024-09-05 17:47:10 +0200

    random import & random phone number display

--- course/bank/bank.go
+++ course/bank/bank.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"course.go/bank/fileops"
+	"github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
@@ -19,6 +20,7 @@ func main() {
 	}
 
 	fmt.Println("Welcome to GoBank")
+	fmt.Println("Reach us 24/7: ", randomdata.PhoneNumber())
 
 	for {
 		presentOptions()
